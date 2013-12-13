0d2cd43849eca707a50fdea95bd63c38966eb0db
Pierre-Jean LEGER <leger.pierrejean@orange.fr>
2013-12-13 14:59:12 +0100

    LOL

--- src/ror/core/actions/MoveAction.java
+++ src/ror/core/actions/MoveAction.java
@@ -3,10 +3,26 @@ package ror.core.actions;
 import ror.core.Rail;
 import ror.core.Robot;
 
+/**
+ * Classe MoveAction représente un déplacement d'un robot
+ * 
+ */
 public class MoveAction extends Action {
     private Rail previous;
     private Rail next;
 
+    /**
+     * Constructeur de la classe InputAction
+     * 
+     * @param duration
+     *            La durée de l'action
+     * @param robot
+     *            Le robot associé à l'action
+     * @param previous
+     *            La rail sur lequel doit se trouver le robot pour effectuer l'action
+     * @param next
+     *            La rail vers lequel se déplace le robot
+     */
     public MoveAction(Integer duration, Robot robot, Rail previous, Rail next) {
 	super(duration, robot);
 	this.setNext(next);
