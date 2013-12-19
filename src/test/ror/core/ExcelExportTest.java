08f79306fec3f0e62e713424c61f5483a43c07e6
Benjamin Grima <grima.benjamin@gmail.com>
2013-12-19 22:03:11 +0100

    début d’ajout de la génération de fichier excel

---
+++ src/test/ror/core/ExcelExportTest.java
@@ -0,0 +1,39 @@
+package test.ror.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ror.core.ExcelExport;
+import ror.core.SimulationManager;
+
+public class ExcelExportTest {
+
+    private ExcelExport excelExport;
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testExcelExport() {
+	excelExport=new ExcelExport(new SimulationManager());
+	//fail("Not yet implemented");
+    }
+
+}
