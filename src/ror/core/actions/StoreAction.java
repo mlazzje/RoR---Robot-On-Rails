e1a53cf09c3423d7b17e52cd13d09e4d4d3e8fef
mlazzje <mlazzje@mlazzje>
2013-12-30 11:28:33 +0100

    StoreAction javadoc

--- src/ror/core/actions/StoreAction.java
+++ src/ror/core/actions/StoreAction.java
@@ -6,27 +6,53 @@ import ror.core.Robot;
 
 public class StoreAction extends Action {
 
+    /**
+     * drawer
+     */
     private Drawer drawer;
+    /**
+     * product
+     */
     private Product product;
 
+    /**
+     * Constructor storeAction
+     * 
+     * @param duration
+     * @param robot
+     * @param drawer
+     * @param product
+     */
     public StoreAction(Integer duration, Robot robot, Drawer drawer, Product product) {
 	super(duration, robot);
 	this.setDrawer(drawer);
 	this.setProduct(product);
     }
 
+    /**
+     * @return drawer
+     */
     public Drawer getDrawer() {
 	return drawer;
     }
 
+    /**
+     * @param drawer
+     */
     public void setDrawer(Drawer drawer) {
 	this.drawer = drawer;
     }
 
+    /**
+     * @return product
+     */
     public Product getProduct() {
 	return product;
     }
 
+    /**
+     * @param product
+     */
     public void setProduct(Product product) {
 	this.product = product;
     }
