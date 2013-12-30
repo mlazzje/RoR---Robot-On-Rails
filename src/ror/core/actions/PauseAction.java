e4380000b19bc876a927bf97922ef92bc3f70c5d
mlazzje <mlazzje@mlazzje>
2013-12-30 11:27:22 +0100

    PauseAction javadoc

--- src/ror/core/actions/PauseAction.java
+++ src/ror/core/actions/PauseAction.java
@@ -4,17 +4,33 @@ import ror.core.Robot;
 
 public class PauseAction extends Action {
 
+	/**
+	 * waiting Robot
+	 */
 	private Robot waitingRobot;
 	
+	/**
+	 * Method to put robot in pause
+	 * 
+	 * @param duration
+	 * @param robot
+	 * @param waitingRobot
+	 */
 	public PauseAction(Integer duration, Robot robot, Robot waitingRobot) {
 		super(duration, robot);
 		this.setWaitingRobot(waitingRobot);
 	}
 
+	/**
+	 * @return robot
+	 */
 	public Robot getWaitingRobot() {
 		return waitingRobot;
 	}
 
+	/**
+	 * @param waitingRobot
+	 */
 	public void setWaitingRobot(Robot waitingRobot) {
 		this.waitingRobot = waitingRobot;
 	}
