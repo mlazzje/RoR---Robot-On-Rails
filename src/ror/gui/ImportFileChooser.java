c10b56698d780f7742616b22ebf6cf1f6504bf80
mlazzje <mlazzje@mlazzje>
2013-12-30 11:42:53 +0100

    ImportFileChooser javadoc

--- src/ror/gui/ImportFileChooser.java
+++ src/ror/gui/ImportFileChooser.java
@@ -2,8 +2,17 @@ package ror.gui;
 
 import javax.swing.JFileChooser;
 
+/**
+ * @author RoR
+ * 
+ *  //TODO TO DELETE This class ?
+ *
+ */
 public class ImportFileChooser extends JFileChooser {
 
+	/**
+	 * serialVersionUID
+	 */
 	private static final long serialVersionUID = 1L;
 
 }
