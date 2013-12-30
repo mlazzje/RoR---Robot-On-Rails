9164e5d479941da92eabb17e80b5561ffda26388
mlazzje <mlazzje@mlazzje>
2013-12-30 11:43:41 +0100

    LogList

--- src/ror/gui/LogList.java
+++ src/ror/gui/LogList.java
@@ -2,11 +2,21 @@ package ror.gui;
 
 import javax.swing.JList;
 
+/**
+ * @author RoR
+ *
+ */
 @SuppressWarnings("rawtypes")
 public class LogList extends JList {
 
+	/**
+	 * serialVersionUID
+	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Constructor LogList
+	 */
 	@SuppressWarnings("unchecked")
 	public LogList() {
 		super();
