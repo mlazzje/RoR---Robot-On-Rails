18dc432480adcbb390295f119bfbcffafd31ef89
mlazzje <mlazzje@mlazzje>
2013-12-30 11:25:56 +0100

    OutputAction Javadoc

--- src/ror/core/actions/OutputAction.java
+++ src/ror/core/actions/OutputAction.java
@@ -6,26 +6,55 @@ import ror.core.Robot;
 
 public class OutputAction extends Action {
 
+	/**
+	 * product
+	 */
 	private Product product;
+	/**
+	 * output spot
+	 */
 	private Output output;
 
+	/**
+	 * Output action
+	 * 
+	 * @param duration
+	 * @param robot
+	 * @param output
+	 */
 	public OutputAction(Integer duration, Robot robot, Output output) {
 		super(duration, robot);
 		this.output = output;
 	}
 
+	/**
+	 * @return product
+	 */
 	public Product getProduct() {
 		return product;
 	}
 
+	/**
+	 * Set product
+	 * 
+	 * @param product
+	 */
 	public void setProduct(Product product) {
 		this.product = product;
 	}
 
+	/**
+	 * @return output
+	 */
 	public Output getOutput() {
 	    return this.output;
 	}
 
+	/**
+	 * Set output
+	 * 
+	 * @param output
+	 */
 	public void setOutput(Output output) {
 	    this.output = output;
 	}
