4c62b16d72bbf4993b430e8a1af35bc8ac4eb29f
mlazzje <mlazzje@mlazzje>
2013-12-30 11:47:21 +0100

    OrderList javadoc

--- src/ror/gui/OrderList.java
+++ src/ror/gui/OrderList.java
@@ -2,10 +2,22 @@ package ror.gui;
 
 import javax.swing.JTable;
 
+/**
+ * @author RoR
+ *
+ */
 public class OrderList extends JTable {
 
+	/**
+	 * serialVersionUID
+	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Constructor OrderList
+	 * 
+	 * @param frame
+	 */
 	public OrderList(RoRFrame frame) {
 		super();
 		this.setBounds(200, 200, 400, 400);
