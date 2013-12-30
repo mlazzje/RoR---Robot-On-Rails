c5fa5b8e40ac3f1811f3faee087e4e9eb055286e
mlazzje <mlazzje@mlazzje>
2013-12-30 11:44:41 +0100

    LogListModel javadoc

--- src/ror/gui/LogListModel.java
+++ src/ror/gui/LogListModel.java
@@ -2,11 +2,21 @@ package ror.gui;
 
 import javax.swing.DefaultListModel;
 
+/**
+ * @author RoR
+ *
+ */
 @SuppressWarnings("rawtypes")
 public class LogListModel extends DefaultListModel {
 	
+	/**
+	 * serialVersionUID
+	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Constructor LogListModel
+	 */
 	public LogListModel() {
 	super();
     }
