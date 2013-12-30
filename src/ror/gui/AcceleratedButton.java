6435102182483c651ee9ffbb5ea0f73abfb28ff9
mlazzje <mlazzje@mlazzje>
2013-12-30 11:38:09 +0100

    AcceleratedButton javadoc

--- src/ror/gui/AcceleratedButton.java
+++ src/ror/gui/AcceleratedButton.java
@@ -9,14 +9,23 @@ import javax.swing.JButton;
 
 public class AcceleratedButton extends JButton implements MouseListener {
 
+	/**
+	 * serialVersionUID
+	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Constructor AcceleratedButton
+	 */
 	public AcceleratedButton() {
 		super();
 		ImageIcon icon = new ImageIcon(new ImageIcon(StartButton.class.getResource("/ressources/accelerated.png")).getImage().getScaledInstance(32, 32, Image.SCALE_DEFAULT));
 		this.setIcon(icon);
 	}
 
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mouseClicked(java.awt.event.MouseEvent)
+	 */
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		if (this.getParent().getParent().getParent().getParent() instanceof RoRFrame) {
@@ -27,18 +36,30 @@ public class AcceleratedButton extends JButton implements MouseListener {
 		}
 	}
 
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mousePressed(java.awt.event.MouseEvent)
+	 */
 	@Override
 	public void mousePressed(MouseEvent e) {
 	}
 
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mouseReleased(java.awt.event.MouseEvent)
+	 */
 	@Override
 	public void mouseReleased(MouseEvent e) {
 	}
 
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mouseEntered(java.awt.event.MouseEvent)
+	 */
 	@Override
 	public void mouseEntered(MouseEvent e) {
 	}
 
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mouseExited(java.awt.event.MouseEvent)
+	 */
 	@Override
 	public void mouseExited(MouseEvent e) {
 	}
