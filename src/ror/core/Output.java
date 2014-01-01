940542f033cc23ce41ddeff7c3bef70187c81bcd
Caligone <leger.pierrejean@orange.fr>
2014-01-01 18:08:57 +0100

    Suppression des produits dans l'output uniquement si la commande est terminée.

--- src/ror/core/Output.java
+++ src/ror/core/Output.java
@@ -57,12 +57,13 @@ public class Output extends RoRElement {
 	synchronized (this.productList) {
 	    boolean returned = this.productList.add(p);
 	    p.setStatus(Product.DONE);
-	    new Timer().schedule(new TimerTask() {          
-	        @Override
-	        public void run() {
-	            Output.this.removeProduct(p);    
-	        }
-	    }, 5000);
+	    if(p.getOrder().getStatus() == Order.DONE)
+		    new Timer().schedule(new TimerTask() {          
+		        @Override
+		        public void run() {
+		            Output.this.removeProduct(p);    
+		        }
+		    }, 5000);
 	    this.setChanged();
 	    this.notifyObservers();
 	    return returned;
