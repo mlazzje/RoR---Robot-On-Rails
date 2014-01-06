b6f13d9a096d3ce415eb6da68fda048ba6be4ff1
Benjamin Grima <grima.benjamin@gmail.com>
2014-01-06 10:12:59 +0100

    modification de la progressbox

--- src/ror/gui/ProgressBox.java
+++ src/ror/gui/ProgressBox.java
@@ -1,37 +1,41 @@
 package ror.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
+import javax.swing.WindowConstants;
 
 public class ProgressBox extends JDialog {
 
-	private static final long serialVersionUID = 1L;
-	private JProgressBar progressBar;
+    private static final long serialVersionUID = 1L;
+    private JProgressBar progressBar;
 
-	public ProgressBox(RoRFrame rorFrame) {
-		super(rorFrame);
-		this.setLayout(new BorderLayout());
-		this.setTitle("Calcul en cours");
-		this.setLocationRelativeTo(null);
-		this.setAlwaysOnTop(true);
-		this.setResizable(false);
-		progressBar = new JProgressBar(0, 100);
-		progressBar.setValue(0);
-		progressBar.setStringPainted(true);
+    public ProgressBox(RoRFrame rorFrame) {
+	//super(rorFrame, "Calcul en cours", true);
+	super(rorFrame);
+	this.setLayout(new BorderLayout());
+	this.setAlwaysOnTop(true);
+	this.setResizable(false);
+	this.setPreferredSize(new Dimension(500, 60));
+	progressBar = new JProgressBar(0, 100);
+	progressBar.setValue(0);
+	progressBar.setStringPainted(true);
+	progressBar.setPreferredSize(new Dimension(500, 40));
+	JPanel panel = new JPanel();
+	panel.add(progressBar);
+	this.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+	add(panel, BorderLayout.PAGE_START);
+	rorFrame.setEnabled(false);
+	this.pack();
+	this.setLocationRelativeTo(null);
+	this.repaint();
+	this.setVisible(true);
+    }
 
-		JPanel panel = new JPanel();
-		panel.add(progressBar);
-
-		add(panel, BorderLayout.PAGE_START);
-		this.pack();
-		this.repaint();
-		this.setVisible(true);
-	}
-
-	public void setProgressValue(int value) {
-		this.progressBar.setValue(value);
-	}
+    public void setProgressValue(int value) {
+	this.progressBar.setValue(value);
+    }
 }
